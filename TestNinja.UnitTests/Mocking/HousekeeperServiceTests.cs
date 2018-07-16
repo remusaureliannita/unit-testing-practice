ee3e0ff8215d9c7d2e0e2b73292a96eda5ece58a
Remus Nita <remus.nita@maxcode.net>
2018-07-16 16:56:06 +0300

    Housekeeper testing

---
+++ TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class HousekeeperServiceTests
+    {
+        private HousekeeperService _service;
+        private Mock<IStatementGenerator> _statementGenerator;
+        private Mock<IEmailSender> _emailSender;
+        private Mock<IXtraMessageBox> _messageBox;
+        private DateTime _statementDate = new DateTime(2017, 1, 1);
+        private Housekeeper _housekeeper;
+        private string _statementFileName;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _housekeeper = new Housekeeper {Email = "a", FullName = "b", Oid = 1, StatementEmailBody = "c"};
+
+            var repository = new Mock<IHousekeeperRepository>();
+            repository.Setup(r => r.GetHousekeepers()).Returns(new List<Housekeeper>
+            {
+                _housekeeper
+            }.AsQueryable());
+
+            _statementFileName = "filename";
+            _statementGenerator = new Mock<IStatementGenerator>();
+            _statementGenerator
+                .Setup(sg => sg.SaveStatement(_housekeeper.Oid, _housekeeper.FullName, _statementDate))
+                .Returns(() => _statementFileName);
+
+            _emailSender = new Mock<IEmailSender>();
+            _messageBox = new Mock<IXtraMessageBox>();
+
+            _service = new HousekeeperService(
+                repository.Object,
+                _statementGenerator.Object,
+                _emailSender.Object,
+                _messageBox.Object);
+
+        }
+
+        [Test]
+        public void SendStatementEmails_WhenCalled_GenerateStatements()
+        {
+            _service.SendStatementEmails(_statementDate);
+
+            _statementGenerator.Verify(sg => sg.SaveStatement(_housekeeper.Oid, _housekeeper.FullName, _statementDate));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void SendStatementEmails_HousekeepersEmailIsInvalid_ShouldNotGenerateStatement(string email)
+        {
+            _housekeeper.Email = email;
+            _service.SendStatementEmails(_statementDate);
+
+            _statementGenerator.Verify(sg => 
+                sg.SaveStatement(_housekeeper.Oid, _housekeeper.FullName, _statementDate), Times.Never);
+        }
+
+        [Test]
+        public void SendStatementEmails_WhenCalled_EmailTheStatement()
+        {
+            _service.SendStatementEmails(_statementDate);
+
+            _emailSender.Verify(es => es.EmailFile(_housekeeper.Email,
+                _housekeeper.StatementEmailBody,
+                _statementFileName,
+                It.IsAny<string>()));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void SendStatementEmails_FileNameIsInvalid_ShouldNotEmailTheStatement(string fileName)
+        {
+            _statementFileName = fileName;
+
+            _service.SendStatementEmails(_statementDate);
+
+            VerifyEmailNotSent();
+        }
+
+        [Test]
+        public void SendStatementEmails_EmailSendingFails_DisplayAMessageBox()
+        {
+            _emailSender.Setup(es => es.EmailFile(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()
+            )).Throws<Exception>();
+
+            _service.SendStatementEmails(_statementDate);
+
+            _messageBox.Verify(mb => mb.Show(It.IsAny<string>(), It.IsAny<string>(), MessageBoxButtons.OK));
+        }
+
+        private void VerifyEmailNotSent()
+        {
+            _emailSender.Verify(es => es.EmailFile(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()),
+                Times.Never);
+        }
+    }
+}
---
+++ TestNinja/Mocking/HousekeeperRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace TestNinja.Mocking
+{
+    public interface IHousekeeperRepository
+    {
+        IQueryable<Housekeeper> GetHousekeepers();
+    }
+
+    public class HousekeeperRepository : IHousekeeperRepository
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public HousekeeperRepository()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public IQueryable<Housekeeper> GetHousekeepers()
+        {
+            var housekeepers = _unitOfWork.Query<Housekeeper>();
+
+            return housekeepers;
+        }
+    }
+}
